bf1cf0b07196a278c6a1825b6026832dcef471d9
Rushil <rishiv001@gmail.com>
2025-05-30 16:24:22 +0530

    Serires3

--- Server/main.go
+++ Server/main.go
@@ -13,6 +13,10 @@ func getResponse(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is a GET request response")
 }
 
+func postResponse(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is a POST request response")
+}
+
 func main() {
 	http.ListenAndServe(":8080", nil)
 	fmt.Println("Server is running on port 8080")
